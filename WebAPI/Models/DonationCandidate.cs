5b9ae0b61db578900b0ad0a13834e6570e97cde0
Han Chien <hanchienleow1@gmail.com>
2022-03-23 16:30:14 +1100

    Schema created

--- WebAPI/Models/DonationCandidate.cs
+++ WebAPI/Models/DonationCandidate.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace WebAPI.Models
 {
     public class DonationCandidate
     {
-        public DonationCandidate()
-        {
-        }
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string fullName { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string mobile { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string email { get; set; }
+
+        public int age { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string bloodGroup { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string address { get; set; }
+
     }
 }
